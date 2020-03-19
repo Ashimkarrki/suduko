26fc413e52fd40fef33ac823f57327d3b65e8dd1
Ashimkarrki <52632464+Ashimkarrki@users.noreply.github.com>
2020-03-19 12:35:39 +0545

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,25 +4,25 @@ using namespace std;
 
 
 int main()
-{           int org[4][4];
-                for(int i=0;i<5;i++){
-                        for(int j=0;j<5;j++){org[i][j]=0;}}
-
-            int r,c;
-            for(int i=0;i<25;i++){int n;
+{           int org[5][5];
+                for(int i=0;i<6;i++){
+                        for(int j=0;j<6;j++){org[i][j]=0;}}
+                          int desicion=1;
+        while(desicion==1){int n;
+             int r,c;
                                 cin>>r>>c;
                                 cin>>n;
                                 org[r-1][c-1]=n;
+
+
                                 cout<<"want more (1-0)?"<<endl;
-                                int desicion;
                                 cin>>desicion;
-                                if (desicion==0){break;}
             }
-            for(int i=0;i<5;i++){
-                for(int j=0;j<5;j++){cout<<org[i][j]<<" " ;}cout<<""<<endl;;
+            for(int i=0;i<6;i++){
+                for(int j=0;j<6;j++){cout<<org[j][i]<<" " ;}cout<<""<<endl;;
             }
 
-
+return 0;
 
 
 
