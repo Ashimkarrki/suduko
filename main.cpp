2711b3c72ed420ef7afdaa110e5480d69960a20a
Ashimkarrki <52632464+Ashimkarrki@users.noreply.github.com>
2020-03-19 11:37:37 +0545

    setup

--- main.cpp
+++ main.cpp
@@ -1,41 +1,31 @@
 #include <iostream>
 
 using namespace std;
-int* divide(){
-            int a;
-            int b;
-            int* asum=new int[50];
-            cout<<"make the pattern"<<endl;
-            for(int i=0;i<50;i=i+2){
-              int putIt;
-                  cin>>putIt;
-                  a=putIt%10;
-                  b=putIt/10;
-                  if(putIt>10){
-                  int temp;
-                  temp=a;
-                  a=b;
-                  b=temp;
-                  }
-                  asum[i]=&a;
-                  asum[i+1]=&b;
-                  cout<<"want more (1-0)"<<endl;
-                  int desicion;
-                  cin>>desicion;
-                  if (desicion==0){break;}
-            }
 
-return asum;
-
-
-}
 
 int main()
-{               int pop[50];
+{           int org[5][5];
+                for(int i=0;i<5;i++){
+                        for(int j=0;j<5;j++){org[i][j]=0;}}
 
-           for(i=0;i<50;i++){
-                    pop[i]=divide();
-           }
+            int r,c;
+            for(int i=0;i<25;i++){int n;
+                                cin>>r>>c;
+                                cin>>n;
+                                org[r][c]=n;
+                                cout<<"want more (1-0)?"<<endl;
+                                int desicion;
+                                cin>>desicion;
+                                if (desicion==0){break;}
+            }
+            for(int i=0;i<5;i++){
+                for(int j=0;j<5;j++){cout<<org[i][j]<<" " ;}cout<<""<<endl;;
+            }
+
+
+
+
+
 
 
 }
